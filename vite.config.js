9167fa3a95af17343653055a2849de51fc312879
wdebusschere <info@e-xperience.be>
2025-02-10 10:22:42 +0000

    feat(init): tailwind 3 vite

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+
+
+import { defineConfig } from "vite";
+import tailwindcss from "tailwindcss";
+export default defineConfig({
+
+plugins: [],
+  css: {
+   postcss: {
+    plugins: [tailwindcss()],
+   },
+  },
+});
